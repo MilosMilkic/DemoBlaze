a066f4cbea4d3ffb9db004954837e12fe2bd2148
QA1.6 <milosmilkic445@gmail.com>
2023-07-11 21:08:15 +0200

    AddFirsTest1

---
+++ src/main/java/IndexPage.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IndexPage extends  BasePage{
+
+
+
+    @FindBy(linkText = "Samsung galaxy s6")
+    WebElement samsungS6;
+
+    @FindBy(css = ".btn.btn-success.btn-lg")
+    WebElement addTooCart;
+
+    public IndexPage(ChromeDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickSamsungS6()
+    {
+        samsungS6.click();
+    }
+
+    public void addToCartSamsungS6()
+    {
+        addTooCart.click();
+
+    }
+}
